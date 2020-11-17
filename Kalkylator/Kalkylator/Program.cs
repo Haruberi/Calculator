3d74f904ff4cfce5c7481342d20f920aeee1f282
Anna <anna.h.hallberg.92@gmail.com>
2020-11-17 17:06:52 +0100

    Change to appropriate name and text

--- Kalkylator/Kalkylator/Program.cs
+++ Kalkylator/Kalkylator/Program.cs
@@ -16,7 +16,7 @@ namespace Kalkylator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CalculatorForm());
         }
     }
 }
